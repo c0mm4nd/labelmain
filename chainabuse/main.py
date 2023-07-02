013adbfa19b3fef92463d70f5f943e32438f6b87
Command <maoxs2@gmail.com>
2023-07-02 11:48:22 +0800

    update chainabuse

--- chainabuse/main.py
+++ chainabuse/main.py
@@ -1,9 +1,11 @@
-# %% 
+# %%
 import time
 import os
 import requests
-from pymongo import MongoClient
-from dotenv import load_dotenv # pip install python-dotenv
+import json
+from pymongo import MongoClient, UpdateOne
+from dotenv import load_dotenv  # pip install python-dotenv
+
 
 def get_50_reports_after_cursor(cur=None):
     # resp after cur # not included
@@ -13,14 +15,11 @@ def get_50_reports_after_cursor(cur=None):
             "input": {
                 "chains": [],
                 "scamCategories": [],
-                "orderBy": {
-                    "field": "CREATED_AT",
-                    "direction": "ASC"
-                }
+                "orderBy": {"field": "CREATED_AT", "direction": "ASC"},
             },
-            "first": 50, # MAX
+            "first": 50,  # MAX
         },
-        "query": "query GetReports($input: ReportsInput, $after: String, $before: String, $last: Float, $first: Float) {\n  reports(\n    input: $input\n    after: $after\n    before: $before\n    last: $last\n    first: $first\n  ) {\n    pageInfo {\n      hasNextPage\n      hasPreviousPage\n      startCursor\n      endCursor\n      __typename\n    }\n    edges {\n      cursor\n      node {\n        ...Report\n        __typename\n      }\n      __typename\n    }\n    count\n    totalCount\n    __typename\n  }\n}\n\nfragment Report on Report {\n  id\n  isPrivate\n  ...ReportPreviewDetails\n  ...ReportAccusedScammers\n  ...ReportAuthor\n  ...ReportAddresses\n  ...ReportEvidences\n  ...ReportCompromiseIndicators\n  ...ReportTokenIDs\n  ...ReportTransactionHashes\n  __typename\n}\n\nfragment ReportPreviewDetails on Report {\n  createdAt\n  scamCategory\n  categoryDescription\n  biDirectionalVoteCount\n  viewerDidVote\n  description\n  lexicalSerializedDescription\n  commentsCount\n  source\n  checked\n  __typename\n}\n\nfragment ReportAccusedScammers on Report {\n  accusedScammers {\n    id\n    info {\n      id\n      contact\n      type\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment ReportAuthor on Report {\n  reportedBy {\n    id\n    username\n    trusted\n    __typename\n  }\n  __typename\n}\n\nfragment ReportAddresses on Report {\n  addresses {\n    id\n    address\n    chain\n    domain\n    label\n    __typename\n  }\n  __typename\n}\n\nfragment ReportEvidences on Report {\n  evidences {\n    id\n    description\n    photo {\n      id\n      name\n      description\n      url\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment ReportCompromiseIndicators on Report {\n  compromiseIndicators {\n    id\n    type\n    value\n    __typename\n  }\n  __typename\n}\n\nfragment ReportTokenIDs on Report {\n  tokens {\n    id\n    tokenId\n    __typename\n  }\n  __typename\n}\n\nfragment ReportTransactionHashes on Report {\n  transactionHashes {\n    id\n    hash\n    chain\n    label\n    __typename\n  }\n  __typename\n}\n"
+        "query": "query GetReports($input: ReportsInput, $after: String, $before: String, $last: Float, $first: Float) {\n  reports(\n    input: $input\n    after: $after\n    before: $before\n    last: $last\n    first: $first\n  ) {\n    pageInfo {\n      hasNextPage\n      hasPreviousPage\n      startCursor\n      endCursor\n      __typename\n    }\n    edges {\n      cursor\n      node {\n        ...Report\n        __typename\n      }\n      __typename\n    }\n    count\n    totalCount\n    __typename\n  }\n}\n\nfragment Report on Report {\n  id\n  isPrivate\n  ...ReportPreviewDetails\n  ...ReportAccusedScammers\n  ...ReportAuthor\n  ...ReportAddresses\n  ...ReportEvidences\n  ...ReportCompromiseIndicators\n  ...ReportTokenIDs\n  ...ReportTransactionHashes\n  __typename\n}\n\nfragment ReportPreviewDetails on Report {\n  createdAt\n  scamCategory\n  categoryDescription\n  biDirectionalVoteCount\n  viewerDidVote\n  description\n  lexicalSerializedDescription\n  commentsCount\n  source\n  checked\n  __typename\n}\n\nfragment ReportAccusedScammers on Report {\n  accusedScammers {\n    id\n    info {\n      id\n      contact\n      type\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment ReportAuthor on Report {\n  reportedBy {\n    id\n    username\n    trusted\n    __typename\n  }\n  __typename\n}\n\nfragment ReportAddresses on Report {\n  addresses {\n    id\n    address\n    chain\n    domain\n    label\n    __typename\n  }\n  __typename\n}\n\nfragment ReportEvidences on Report {\n  evidences {\n    id\n    description\n    photo {\n      id\n      name\n      description\n      url\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment ReportCompromiseIndicators on Report {\n  compromiseIndicators {\n    id\n    type\n    value\n    __typename\n  }\n  __typename\n}\n\nfragment ReportTokenIDs on Report {\n  tokens {\n    id\n    tokenId\n    __typename\n  }\n  __typename\n}\n\nfragment ReportTransactionHashes on Report {\n  transactionHashes {\n    id\n    hash\n    chain\n    label\n    __typename\n  }\n  __typename\n}\n",
     }
 
     if cur is not None:
@@ -29,36 +28,28 @@ def get_50_reports_after_cursor(cur=None):
     resp = None
     while True:
         try:
-            resp = requests.post("https://www.chainabuse.com/api/graphql-proxy", json=data)
+            resp = requests.post(
+                "https://www.chainabuse.com/api/graphql-proxy", json=data
+            )
             ret = resp.json()
-            if ret.get("data") is None:
-                print(ret)
-                time.sleep(10)
-                continue
-
-            if ret["data"].get("reports") is None:
-                print(ret)
-                time.sleep(10)
-                continue
 
             return ret
         except Exception as e:
-            if resp is not None:
-                print(resp.text)
-            print(e)
+            print(f"from {cur}, result is not json")
+            with open(f"error_{time.now()}.log", "w") as f:
+                f.write(resp.text)
             time.sleep(10)
 
-    
 
 def has_next(result):
+    if result is None:
+        return True  # means start
+
     if result["data"]["reports"].get("pageInfo") is None:
         return False
-    
+
     return result["data"]["reports"]["pageInfo"]["hasNextPage"] == True
 
-def upsert_report(coll, report):
-    return coll.update_one(filter={"node.id": report["node"]["id"]}, update={"$set": report}, upsert=True)
- 
 
 # %%
 if __name__ == "__main__":
@@ -68,22 +59,49 @@ if __name__ == "__main__":
     coll = db["chainAbuse"]
 
     cur = None
-    result = get_50_reports_after_cursor()
+    result = None
+    total_inserted = 0
+    total_modified = 0
     while True:
         if has_next(result):
-            time.sleep(2)
+            result = get_50_reports_after_cursor(cur)
+            if result.get("data") is None:
+                print(f"from {cur}, result has no data")
+                with open(f"error_{time.now()}.log", "w") as f:
+                    json.dump(result, f)
+                time.sleep(60)
+                continue
+
+            if result["data"].get("reports") is None:
+                print(f"from {cur}, result has no reports")
+                with open(f"error_{time.now()}.log", "w") as f:
+                    json.dump(result, f)
+                time.sleep(600)
+                continue
+
             cur = result["data"]["reports"]["pageInfo"]["endCursor"]
 
-            inserted_count = 0
-            modified_count = 0
+            bulk = []
             for report in result["data"]["reports"]["edges"]:
-                upsert_report(coll, report)
-                inserted_count +=  result.inserted_count 
-                modified_count += result.modified_count
+                bulk.append(
+                    UpdateOne(
+                        filter={"node.id": report["node"]["id"]},
+                        update={"$set": report},
+                        upsert=True,
+                    )
+                )
+            result = coll.bulk_write(requests=bulk)
+            total_inserted += result.inserted_count
+            total_modified += result.modified_count
 
-            result = get_50_reports_after_cursor(cur)
-
-            print("done: {} inserted, {} updated".format(inserted_count, modified_count))
+            print(
+                "done: inserted {}/{}, modified {}/{}".format(
+                    result.inserted_count,
+                    total_inserted,
+                    result.modified_count,
+                    total_modified,
+                )
+            )
         else:
             print("updated to latest")
             time.sleep(3600)
