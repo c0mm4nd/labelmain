5a62fcbc690166fd1442a4d9c3c131c5c3aa4a63
Command <maoxs2@gmail.com>
2023-07-02 11:13:12 +0800

    change order to asc

--- chainabuse/main.py
+++ chainabuse/main.py
@@ -15,11 +15,10 @@ def get_50_reports_after_cursor(cur=None):
                 "scamCategories": [],
                 "orderBy": {
                     "field": "CREATED_AT",
-                    "direction": "DESC"
+                    "direction": "ASC"
                 }
             },
             "first": 50, # MAX
-            
         },
         "query": "query GetReports($input: ReportsInput, $after: String, $before: String, $last: Float, $first: Float) {\n  reports(\n    input: $input\n    after: $after\n    before: $before\n    last: $last\n    first: $first\n  ) {\n    pageInfo {\n      hasNextPage\n      hasPreviousPage\n      startCursor\n      endCursor\n      __typename\n    }\n    edges {\n      cursor\n      node {\n        ...Report\n        __typename\n      }\n      __typename\n    }\n    count\n    totalCount\n    __typename\n  }\n}\n\nfragment Report on Report {\n  id\n  isPrivate\n  ...ReportPreviewDetails\n  ...ReportAccusedScammers\n  ...ReportAuthor\n  ...ReportAddresses\n  ...ReportEvidences\n  ...ReportCompromiseIndicators\n  ...ReportTokenIDs\n  ...ReportTransactionHashes\n  __typename\n}\n\nfragment ReportPreviewDetails on Report {\n  createdAt\n  scamCategory\n  categoryDescription\n  biDirectionalVoteCount\n  viewerDidVote\n  description\n  lexicalSerializedDescription\n  commentsCount\n  source\n  checked\n  __typename\n}\n\nfragment ReportAccusedScammers on Report {\n  accusedScammers {\n    id\n    info {\n      id\n      contact\n      type\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment ReportAuthor on Report {\n  reportedBy {\n    id\n    username\n    trusted\n    __typename\n  }\n  __typename\n}\n\nfragment ReportAddresses on Report {\n  addresses {\n    id\n    address\n    chain\n    domain\n    label\n    __typename\n  }\n  __typename\n}\n\nfragment ReportEvidences on Report {\n  evidences {\n    id\n    description\n    photo {\n      id\n      name\n      description\n      url\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nfragment ReportCompromiseIndicators on Report {\n  compromiseIndicators {\n    id\n    type\n    value\n    __typename\n  }\n  __typename\n}\n\nfragment ReportTokenIDs on Report {\n  tokens {\n    id\n    tokenId\n    __typename\n  }\n  __typename\n}\n\nfragment ReportTransactionHashes on Report {\n  transactionHashes {\n    id\n    hash\n    chain\n    label\n    __typename\n  }\n  __typename\n}\n"
     }
@@ -70,32 +69,21 @@ if __name__ == "__main__":
 
     cur = None
     result = get_50_reports_after_cursor()
-    page = 0
-    while has_next(result):
-        time.sleep(2)
-        cur = result["data"]["reports"]["pageInfo"]["endCursor"]
-
-        for report in result["data"]["reports"]["edges"]:
-            upsert_report(coll, report)
-
-        result = get_50_reports_after_cursor(cur)
-
-        print("done page {}: {}".format(page, len(result["data"]["reports"]["edges"])))
-        page += 1
-    
-    print("done initial load")
-    cur = None # reset cur
     while True:
-        time.sleep(3600)
-        result = get_50_reports_after_cursor()
-
-        while has_next(result):
+        if has_next(result):
             time.sleep(2)
             cur = result["data"]["reports"]["pageInfo"]["endCursor"]
 
+            inserted_count = 0
+            modified_count = 0
             for report in result["data"]["reports"]["edges"]:
-                mongo_result = upsert_report(coll, report)
-                if mongo_result.matched_count == 1:
-                    break
+                upsert_report(coll, report)
+                inserted_count +=  result.inserted_count 
+                modified_count += result.modified_count
 
             result = get_50_reports_after_cursor(cur)
+
+            print("done: {} inserted, {} updated".format(inserted_count, modified_count)))
+        else:
+            print("updated to latest")
+            time.sleep(3600)
