053a5105e58bb8269a7a868db13186b60ebf4e08
Command <me@c0mm4nd.com>
2024-05-08 22:49:19 +0800

    add tronscan

---
+++ tronscan/main.go
@@ -0,0 +1,161 @@
+package tronscan
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/jellydator/ttlcache/v3"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+// https://apilist.tronscanapi.com/api/search/v2?term=token&type=allstart=0&limit=20
+
+func chk(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func retry(err error) bool {
+	if err != nil {
+		log.Println(err)
+		return true
+	}
+
+	return false
+}
+
+var defaultLastSleep = 5 * time.Second
+var lastSleep = 5 * time.Second
+
+var cache = ttlcache.New[string, struct{}](
+	ttlcache.WithTTL[string, struct{}](12 * time.Hour),
+)
+
+type Result struct {
+	Total    int    `json:"total"`
+	Token    []any  `json:"token"`
+	Address  []any  `json:"address"`
+	Contract []any  `json:"contract"`
+	Message  string `json:"message"`
+}
+
+const alphabet = "123456789abcdefghijklmnopqrstuvwxyz_-"
+
+func search_tronscan(database *mongo.Database, search_type, term string, start int) {
+	total := 0
+START_CHANGE:
+	coll := database.Collection(fmt.Sprintf("tron%sLabels", cases.Title(language.English, cases.NoLower).String(search_type)))
+	token_url := fmt.Sprintf("https://apilist.tronscanapi.com/api/search/v2?term=%s&type=%s&start=%d&limit=50", term, search_type, start)
+RETRY:
+	resp, err := http.Get(token_url)
+	if retry(err) {
+		lastSleep += defaultLastSleep
+		time.Sleep(lastSleep)
+		goto RETRY
+	}
+	body, err := io.ReadAll(resp.Body)
+	chk(err)
+
+	var result Result
+	json.Unmarshal(body, &result)
+
+	if result.Message != "" {
+		log.Println(result.Message)
+		time.Sleep(lastSleep)
+		goto RETRY
+	}
+
+	var models []mongo.WriteModel
+	switch search_type {
+	case "token":
+		models = make([]mongo.WriteModel, len(result.Token))
+		for i, item := range result.Token {
+			model := mongo.NewUpdateOneModel().SetUpsert(true).SetFilter(bson.M{"token_id": item.(map[string]any)["token_id"]}).SetUpdate(bson.M{"$set": item})
+			models[i] = model
+		}
+	case "address":
+		models = make([]mongo.WriteModel, len(result.Address))
+		for i, item := range result.Address {
+			model := mongo.NewUpdateOneModel().SetUpsert(true).SetFilter(bson.M{"address": item.(map[string]any)["address"]}).SetUpdate(bson.M{"$set": item})
+			models[i] = model
+		}
+	case "contract":
+		models = make([]mongo.WriteModel, len(result.Contract))
+		for i, item := range result.Contract {
+			model := mongo.NewUpdateOneModel().SetUpsert(true).SetFilter(bson.M{"contract_address": item.(map[string]any)["contract_address"]}).SetUpdate(bson.M{"$set": item})
+			models[i] = model
+		}
+	default:
+		panic("unknown search type")
+	}
+
+	_, err = coll.BulkWrite(context.TODO(), models)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Printf("since %d: %d", start, result.Total)
+
+	total += result.Total
+	if result.Total == 50 {
+		start += 50
+		goto START_CHANGE
+	}
+
+	if total == 0 {
+		return
+	}
+
+	for _, char := range alphabet {
+		nextTerm := term + string(char)
+		search_tronscan(database, search_type, nextTerm, start)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	mongoURI := os.Getenv("MONGO_URI")
+
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Send a ping to confirm a successful connection
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	db := client.Database("labels")
+
+	// check from "" to arbitrarily length string. stop length increase when total is 0
+	search_tronscan(db, "token", "", 0)
+}
